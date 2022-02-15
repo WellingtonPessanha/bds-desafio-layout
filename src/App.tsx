279e495575584cbf65531364278632f21e6888bb
Wellington Pessanha <ricardo.wellington@hotmail.com>
2022-02-15 01:59:24 -0300

    Finished project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import './assets/styles/custom.scss';
 import './App.css';
 
-function App() {
-  return (
-    <h1> Hello Carros Top</h1>    
-  );
-}
+import Routes from 'Routes';
+
+const App = () => {
+  return <Routes />;
+};
 
 export default App;
